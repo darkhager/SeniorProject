5d81088052836d41b17533ab6329011fe4abadce
darkhager <hengdxl@hotmail.com>
2020-03-10 16:27:10 +0700

    Random and Show Alphabet

--- SeniorProject/Assets/WordSystem.cs
+++ SeniorProject/Assets/WordSystem.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class WordSystem : MonoBehaviour
 {
-    Random rand = new Random();
     string alphabetSet = "";
     string[] alphabetCollection = {
         "a", "a", "a", "a", "a", "a", "a", "a", "a",//9
@@ -34,23 +33,60 @@ public class WordSystem : MonoBehaviour
         "x",//1
         "y", "y",//2
         "z" };//1
-    public Text alphaText;
+    public Text alphaText, alphabet1, alphabet2,alphabet3, alphabet4, alphabet5, 
+        alphabet6, alphabet7, alphabet8, alphabet9, alphabet10, 
+        alphabet11, alphabet12, alphabet13, alphabet14, alphabet15
+        ;
 
     void Start()
     {
-        while (alphabetSet.Length != 16) {
+        while (alphabetSet.Length != 15) {
             alphabetSet = alphabetSet + alphabetCollection[UnityEngine.Random.RandomRange(0,alphabetCollection.Length)];
-            alphaText.text = alphabetSet;
         }
+        int count = 0;
+        for (int i = 0; i < 15; i++) {
+            if (alphabetSet[i] == 'a' || alphabetSet[i] == 'e' || alphabetSet[i] == 'i' || alphabetSet[i] == 'o' || alphabetSet[i] == 'u') { }
+        }
+            alphaText.text = alphabetSet;
+            alphabet1.text = new string(alphabetSet[0], 1);
+            alphabet2.text = new string(alphabetSet[1], 1);
+            alphabet3.text = new string(alphabetSet[2], 1);
+            alphabet4.text = new string(alphabetSet[3], 1);
+            alphabet5.text = new string(alphabetSet[4], 1);
+            alphabet6.text = new string(alphabetSet[5], 1);
+            alphabet7.text = new string(alphabetSet[6], 1);
+            alphabet8.text = new string(alphabetSet[7], 1);
+            alphabet9.text = new string(alphabetSet[8], 1);
+            alphabet10.text = new string(alphabetSet[9], 1);
+            alphabet11.text = new string(alphabetSet[10], 1);
+            alphabet12.text = new string(alphabetSet[11], 1);
+            alphabet13.text = new string(alphabetSet[12], 1);
+            alphabet14.text = new string(alphabetSet[13], 1);
+            alphabet15.text = new string(alphabetSet[14], 1);
     }
 
 
     void Update()
     {
-        while (alphabetSet.Length != 16)
+        while (alphabetSet.Length != 15)
         {
-            alphabetSet = alphabetSet + alphabetCollection[UnityEngine.Random.RandomRange(0, alphabetCollection.Length)];
-            alphaText.text = alphabetSet;
+            alphabetSet = alphabetSet + alphabetCollection[UnityEngine.Random.RandomRange(0, alphabetCollection.Length)]; 
         }
+            alphaText.text = alphabetSet;
+            alphabet1.text = new string(alphabetSet[0], 1);
+            alphabet2.text = new string(alphabetSet[1], 1); 
+            alphabet3.text = new string(alphabetSet[2], 1); 
+            alphabet4.text = new string(alphabetSet[3], 1); 
+            alphabet5.text = new string(alphabetSet[4], 1); 
+            alphabet6.text = new string(alphabetSet[5], 1); 
+            alphabet7.text = new string(alphabetSet[6], 1); 
+            alphabet8.text = new string(alphabetSet[7], 1); 
+            alphabet9.text = new string(alphabetSet[8], 1); 
+            alphabet10.text = new string(alphabetSet[9], 1); 
+            alphabet11.text = new string(alphabetSet[10], 1); 
+            alphabet12.text = new string(alphabetSet[11], 1);
+            alphabet13.text = new string(alphabetSet[12], 1);
+            alphabet14.text = new string(alphabetSet[13], 1);
+            alphabet15.text = new string(alphabetSet[14], 1);
     }
 }
