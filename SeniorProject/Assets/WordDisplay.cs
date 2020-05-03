311c79e7252d60bbe9e646ddf16005e3329020bb
darkhager <hengdxl@hotmail.com>
2020-05-04 00:50:33 +0700

    Try to click to get word

--- SeniorProject/Assets/WordDisplay.cs
+++ SeniorProject/Assets/WordDisplay.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class WordDisplay : MonoBehaviour
 {
     public Text wordText;
-    string wordSet = "wordSet";
-    private void Update()
+    string wordSet = "";
+    void Update()
     {
         wordText.text = wordSet;
+    }
+    public string SetWordSet
+    {
+        get { return wordSet; }
+        set { wordSet = value; }
     }
 }
--- SeniorProject/Assets/WordSystem.cs
+++ SeniorProject/Assets/WordSystem.cs
@@ -45,63 +45,113 @@ public class WordSystem : MonoBehaviour
         alphabet6, alphabet7, alphabet8, alphabet9, alphabet10, 
         alphabet11, alphabet12, alphabet13, alphabet14, alphabet15
         ;
-
+    public string alpha1, alpha2, alpha3, alpha4, alpha5, alpha6, alpha7, alpha8,
+        alpha9, alpha10, alpha11, alpha12, alpha13, alpha14, alpha15;
     void Start()
     {
-        while(alphabetSet.Length!=15){
-            alphabetSet = alphabetSet+alphabetCollection[Random.Range(0,alphabetCollection.Length-1)];
-        }
+        AlphabetSetRandom();
     }
 
 
     void Update()
     {
         alphaText.text = alphabetSet;
-        alphabet1.text = alphabetSet.Substring(0,1);
-        alphabet2.text = alphabetSet.Substring(1, 1);
-        alphabet3.text = alphabetSet.Substring(2, 1);
-        alphabet4.text = alphabetSet.Substring(3, 1);
-        alphabet5.text = alphabetSet.Substring(4, 1);
-        alphabet6.text = alphabetSet.Substring(5, 1);
-        alphabet7.text = alphabetSet.Substring(6, 1);
-        alphabet8.text = alphabetSet.Substring(7, 1);
-        alphabet9.text = alphabetSet.Substring(8, 1);
-        alphabet10.text = alphabetSet.Substring(9, 1);
-        alphabet11.text = alphabetSet.Substring(10, 1);
-        alphabet12.text = alphabetSet.Substring(11, 1);
-        alphabet13.text = alphabetSet.Substring(12, 1);
-        alphabet14.text = alphabetSet.Substring(13, 1);
-        alphabet15.text = alphabetSet.Substring(14, 1);
-        string[] arrayCheck= { };
-        int count = 0;
-        for (int i = 0; i < 15; i++) {
-            if (alphabetSet[i] == 'A') {
-                arrayCheck[i] ="A";
-                count++;
-            }
-            if (alphabetSet[i] == 'E')
-            {
-                arrayCheck[i] = "E";
-                count++;
-            }
-            if (alphabetSet[i] == 'I')
-            {
-                arrayCheck[i] = "I";
-                count++;
-            }
-            if (alphabetSet[i] == 'O')
-            {
-                arrayCheck[i] = "O";
-                count++;
-            }
-            if (alphabetSet[i] == 'U')
-            {
-                arrayCheck[i] = "U";
-                count++;
-            }
-        }
-        if (count < 4) {
-            
+        alpha1 = alphabetSet.Substring(0, 1);
+        alphabet1.text = alpha1;
+        alpha2 = alphabetSet.Substring(1, 1);
+        alphabet2.text = alpha2;
+        alpha3 = alphabetSet.Substring(2, 1);
+        alphabet3.text = alpha3;
+        alpha4 = alphabetSet.Substring(3, 1);
+        alphabet4.text = alpha4;
+        alpha5 = alphabetSet.Substring(4, 1);
+        alphabet5.text = alpha5;
+        alpha6 = alphabetSet.Substring(5, 1);
+        alphabet6.text = alpha6;
+        alpha7 = alphabetSet.Substring(6, 1);
+        alphabet7.text = alpha7;
+        alpha8 = alphabetSet.Substring(7, 1);
+        alphabet8.text = alpha8;
+        alpha9 = alphabetSet.Substring(8, 1);
+        alphabet9.text = alpha9;
+        alpha10 = alphabetSet.Substring(9, 1);
+        alphabet10.text = alpha10;
+        alpha11 = alphabetSet.Substring(10, 1);
+        alphabet11.text = alpha11;
+        alpha12 = alphabetSet.Substring(11, 1);
+        alphabet12.text = alpha12;
+        alpha13 = alphabetSet.Substring(12, 1);
+        alphabet13.text = alpha13;
+        alpha14 = alphabetSet.Substring(13, 1);
+        alphabet14.text = alpha14;
+        alpha15 = alphabetSet.Substring(14, 1);
+        alphabet15.text = alpha15;
+        AlphabetSetRandom();
+    }
+    public void AlphabetSetRandom() {
+        while (alphabetSet.Length != 15)
+        {
+            alphabetSet = alphabetSet + alphabetCollection[Random.Range(0, alphabetCollection.Length - 1)];
         }
     }
+    public string GetAlphabet1() {
+        return alpha1;
+    }
+    public string GetAlpha2
+    {
+        get { return alpha2; }
+    }
+    public string GetAlpha3
+    {
+        get { return alpha3; }
+    }
+    public string GetAlpha4
+    {
+        get { return alpha4; }
+    }
+    public string GetAlpha5
+    {
+        get { return alpha5; }
+    }
+        public string GetAlpha6
+    {
+        get { return alpha6; }
+    }
+    public string GetAlpha7
+    {
+        get { return alpha7; }
+    }
+    public string GetAlpha8
+    {
+        get { return alpha8; }
+    }
+    public string GetAlpha9
+    {
+        get { return alpha9; }
+    }
+    public string GetAlpha10
+    {
+        get { return alpha10; }
+    }
+    public string GetAlpha11
+    {
+        get { return alpha11; }
+    }
+    public string GetAlpha12
+    {
+        get { return alpha12; }
+    }
+    public string GetAlpha13
+    {
+        get { return alpha13; }
+    }
+    public string GetAlpha14
+    {
+        get { return alpha14; }
+    }
+    public string GetAlpha15
+    {
+        get { return alpha15; }
+    }
 }
+
