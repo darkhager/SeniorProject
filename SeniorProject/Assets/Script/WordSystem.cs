a785fe819d53e8f302b5fdd5953852241df914d4
darkhager <hengdxl@hotmail.com>
2020-05-21 01:07:11 +0700

    Complete Alphabet Score
    -complete alphabet score 
    -try to do word check function and re-random function

--- SeniorProject/Assets/Script/WordSystem.cs
+++ SeniorProject/Assets/Script/WordSystem.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 
 public class WordSystem : MonoBehaviour
 {
-    string alphabetSet = "", alpha1, alpha2, alpha3, alpha4, alpha5, alpha6, alpha7, alpha8,
-        alpha9, alpha10, alpha11, alpha12, alpha13, alpha14, alpha15, wordString, dictionary;
+    string alphabetSet = "", wordString="", dictionary;
+    string[] alphabetSetCollection = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" };
     string[] alphabetCollection = {
         "A", "A", "A", "A", "A", "A", "A", "A", "A",//9
         "B", "B",//2
@@ -53,207 +53,146 @@ public class WordSystem : MonoBehaviour
         button9, button10, button11, button12, button13, button14, button15,
         refresh, check;
     public TextAsset dictionaryFile;
-    private int scoreCount = 0,score1 = 0, score2 = 0, score3 = 0, score4 = 0
-        , score5 = 0, score6 = 0, score7 = 0, score8 = 0, score9 = 0
-        , score10 = 0, score11 = 0, score12 = 0, score13 = 0, score14 = 0
-        , score15 = 0, scoreControl;
-
+    private int[] scoreControl = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     void Start()
     {
         score.text = "Score : 0";
         dictionary = dictionaryFile.text;
         refresh.interactable = false;
         check.interactable = false;
+        AlphabetSetRandom();
+        AlphaScore();
     }
 
 
     void Update()
     {
-        AlphabetSetRandom();
+        
         alphaText.text = alphabetSet;
+   
+        alphabet1.text = alphabetSetCollection[1];
+        alphabet2.text = alphabetSetCollection[2];
+        alphabet3.text = alphabetSetCollection[3];
+        alphabet4.text = alphabetSetCollection[4];
+        alphabet5.text = alphabetSetCollection[5];
+        alphabet6.text = alphabetSetCollection[6];
+        alphabet7.text = alphabetSetCollection[7];
+        alphabet8.text = alphabetSetCollection[8];
+        alphabet9.text = alphabetSetCollection[9];
+        alphabet10.text = alphabetSetCollection[10];
+        alphabet11.text = alphabetSetCollection[11];
+        alphabet12.text = alphabetSetCollection[12];
+        alphabet13.text = alphabetSetCollection[13];
+        alphabet14.text = alphabetSetCollection[14];
+        alphabet15.text = alphabetSetCollection[15];
 
-        alpha1 = alphabetSet.Substring(0, 1);
-        alphabet1.text = alpha1;
-        alpha2 = alphabetSet.Substring(1, 1);
-        alphabet2.text = alpha2;
-        alpha3 = alphabetSet.Substring(2, 1);
-        alphabet3.text = alpha3;
-        alpha4 = alphabetSet.Substring(3, 1);
-        alphabet4.text = alpha4;
-        alpha5 = alphabetSet.Substring(4, 1);
-        alphabet5.text = alpha5;
-        alpha6 = alphabetSet.Substring(5, 1);
-        alphabet6.text = alpha6;
-        alpha7 = alphabetSet.Substring(6, 1);
-        alphabet7.text = alpha7;
-        alpha8 = alphabetSet.Substring(7, 1);
-        alphabet8.text = alpha8;
-        alpha9 = alphabetSet.Substring(8, 1);
-        alphabet9.text = alpha9;
-        alpha10 = alphabetSet.Substring(9, 1);
-        alphabet10.text = alpha10;
-        alpha11 = alphabetSet.Substring(10, 1);
-        alphabet11.text = alpha11;
-        alpha12 = alphabetSet.Substring(11, 1);
-        alphabet12.text = alpha12;
-        alpha13 = alphabetSet.Substring(12, 1);
-        alphabet13.text = alpha13;
-        alpha14 = alphabetSet.Substring(13, 1);
-        alphabet14.text = alpha14;
-        alpha15 = alphabetSet.Substring(14, 1);
-        alphabet15.text = alpha15;
-
-        AlphaScore(alpha1);
-        score1 = scoreControl;
-        alphabetScore1.text += score1;
-
-        AlphaScore(alpha2);
-        score2 = scoreControl;
-        alphabetScore2.text += score2;
-
-        AlphaScore(alpha3);
-        score3 = scoreControl;
-        alphabetScore1.text += score3;
-
-        AlphaScore(alpha4);
-        score4 = scoreControl;
-        alphabetScore1.text += score4;
-
-        AlphaScore(alpha5);
-        score5 = scoreControl;
-        alphabetScore1.text += score5;
-
-        AlphaScore(alpha6);
-        score6 = scoreControl;
-        alphabetScore1.text += score6;
-
-        AlphaScore(alpha7);
-        score7 = scoreControl;
-        alphabetScore1.text += score7;
-
-        AlphaScore(alpha8);
-        score8 = scoreControl;
-        alphabetScore1.text += score8;
-
-        AlphaScore(alpha9);
-        score9 = scoreControl;
-        alphabetScore1.text += score9;
-
-        AlphaScore(alpha10);
-        score10 = scoreControl;
-        alphabetScore1.text += score10;
-
-        AlphaScore(alpha11);
-        score11 = scoreControl;
-        alphabetScore1.text += score11;
-
-        AlphaScore(alpha12);
-        score12 = scoreControl;
-        alphabetScore1.text += score12;
-
-        AlphaScore(alpha13);
-        score13 = scoreControl;
-        alphabetScore1.text += score13;
-
-        AlphaScore(alpha14);
-        score14 = scoreControl;
-        alphabetScore1.text += score14;
-
-        AlphaScore(alpha15);
-        score15 = scoreControl;
-        alphabetScore1.text += score15;
+        alphabetScore1.text = ""+ scoreControl[1];
+        alphabetScore2.text = "" + scoreControl[2];
+        alphabetScore3.text = "" + scoreControl[3];
+        alphabetScore4.text = "" + scoreControl[4];
+        alphabetScore5.text = "" + scoreControl[5];
+        alphabetScore6.text = "" + scoreControl[6];
+        alphabetScore7.text = "" + scoreControl[7];
+        alphabetScore8.text = "" + scoreControl[8];
+        alphabetScore9.text = "" + scoreControl[9];
+        alphabetScore10.text = "" + scoreControl[10];
+        alphabetScore11.text = "" + scoreControl[11];
+        alphabetScore12.text = "" + scoreControl[12];
+        alphabetScore13.text = "" + scoreControl[13];
+        alphabetScore14.text = "" + scoreControl[14];
+        alphabetScore15.text = "" + scoreControl[15];
 
         word.text = wordString;
     }
 
 
     public void AlphabetSelect1() {
-        wordString += alpha1;
+        wordString += alphabetSetCollection[1];
         button1.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect2()
     {
-        wordString += alpha2;
+        wordString += alphabetSetCollection[2];
         button2.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect3()
     {
-        wordString += alpha3;
+        wordString += alphabetSetCollection[3];
         button3.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect4()
     {
-        wordString += alpha4;
+        wordString += alphabetSetCollection[4];
         button4.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect5()
     {
-        wordString += alpha5;
+        wordString += alphabetSetCollection[5];
         button5.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect6()
     {
-        wordString += alpha6;
+        wordString += alphabetSetCollection[6];
         button6.interactable = false;
         refresh.interactable = true;
         check.interactable = true;
     }
     public void AlphabetSelect7()
     {
-        wordString += alpha7;
+        wordString += alphabetSetCollection[7];
         button7.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect8()
     {
-        wordString += alpha8;
+        wordString += alphabetSetCollection[8];
         button8.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect9()
     {
-        wordString += alpha9;
+        wordString += alphabetSetCollection[9];
         button9.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect10()
     {
-        wordString += alpha10;
+        wordString += alphabetSetCollection[10];
         button10.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect11()
     {
-        wordString += alpha11;
+        wordString += alphabetSetCollection[11];
         button11.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect12()
     {
-        wordString += alpha12;
+        wordString += alphabetSetCollection[12];
         button12.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect13()
     {
-        wordString += alpha13;
+        wordString += alphabetSetCollection[13];
         button13.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect14()
     {
-        wordString += alpha14;
+        wordString += alphabetSetCollection[14];
         button14.interactable = false;
         interactableFunction(true);
     }
     public void AlphabetSelect15()
     {
-        wordString += alpha15;
+        wordString += alphabetSetCollection[15];
         button15.interactable = false;
         interactableFunction(true);
     }
@@ -263,10 +202,13 @@ public class WordSystem : MonoBehaviour
         interactableFunction(false);
     }
     public void CheckDictionary() {
-        if (dictionary.Contains(wordString)) {
-            interactableAlphabet(true);
-            interactableFunction(false);
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < wordString.Length; j++) {
+
+            }
         }
+        interactableAlphabet(true);
+        interactableFunction(false);
     }
     public void interactableAlphabet(bool setBoolean) {
         button1.interactable = setBoolean;
@@ -375,95 +317,98 @@ public class WordSystem : MonoBehaviour
         }
     }
     public void AlphabetSetRandom() {
-        while (alphabetSet.Length != 15)
+        string alphabetSelect = "";
+        for (int i= alphabetSelect.Length ; i < 15 ; i++)
         {
-            alphabetSet = alphabetSet + alphabetCollection[Random.Range(0, alphabetCollection.Length - 1)];
+            alphabetSelect = alphabetCollection[Random.Range(0, alphabetCollection.Length - 1)];
+            alphabetSetCollection[i] = alphabetSelect;
         } 
     }
-    public void AlphaScore(string alpha) {
-        int alphaValue = 0;
-            switch (alpha.ToLower()) {
+    public void AlphaScore() {
+        for(int i = 0;i < 15;i++)
+            switch (alphabetSetCollection[i].ToLower()) {
             case "e":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "a":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "i":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "o":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "n":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "r":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "t":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "l":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "s":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "u":
-                alphaValue = 1;
+                    scoreControl[i] = 1;
                 break;
             case "d":
-                alphaValue = 2;
+                    scoreControl[i] = 2;
                 break;
             case "g":
-                alphaValue = 2;
+                    scoreControl[i] = 2;
                 break;
             case "b":
-                alphaValue = 3;
+                    scoreControl[i] = 3;
                 break;
             case "c":
-                alphaValue = 3;
+                    scoreControl[i] = 3;
                 break;
             case "m":
-                alphaValue = 3;
+                    scoreControl[i] = 3;
                 break;
             case "p":
-                alphaValue = 3;
+                    scoreControl[i] = 3;
                 break;
             case "f":
-                alphaValue = 4;
+                    scoreControl[i] = 4;
                 break;
             case "h":
-                alphaValue = 4;
+                    scoreControl[i] = 4;
                 break;
             case "v":
-                alphaValue = 4;
+                    scoreControl[i] = 4;
                 break;
             case "w":
-                alphaValue = 4;
+                    scoreControl[i] = 4;
                 break;
             case "y":
-                alphaValue = 4;
+                    scoreControl[i] = 4;
                 break;
             case "k":
-                alphaValue = 5;
+                    scoreControl[i] = 5;
                 break;
             case "j":
-                alphaValue = 8;
+                    scoreControl[i] = 8;
                 break;
             case "x":
-                alphaValue = 8;
+                    scoreControl[i] = 8;
                 break;
             case "q":
-                alphaValue = 10;
+                    scoreControl[i] = 10;
                 break;
             case "z":
-                alphaValue = 10;
+                    scoreControl[i] = 10;
                 break;
+            default:
+                    scoreControl[i] = 0;
+                    break;
         }
-        Debug.Log(alphaValue);
-        scoreControl = alphaValue;
     }
 }
 
