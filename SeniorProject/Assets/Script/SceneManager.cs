b9222788f2e50297ea20fbfef642eed220dc1838
darkhager <hengdxl@hotmail.com>
2020-11-19 21:50:55 +0700

    Update 1.6

---
+++ SeniorProject/Assets/Script/SceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void TimeTrialMode()
+    {
+
+    }
+
+    public void BossMode()
+    {
+
+    }
+
+    public void Option()
+    {
+
+    }
+
+    public void Credit()
+    {
+
+    }
+
+}
